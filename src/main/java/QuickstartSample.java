bca030652f349e16c4918db6af0e9c045ec220ff
TeamJL7782 <teamjl7782@gmail.com>
2022-02-21 20:38:15 -0500

    Initial commit

---
+++ src/main/java/QuickstartSample.java
@@ -0,0 +1,47 @@
+//To be used when we have an account with Gcloud
+//Source Material - https://cloud.google.com/speech-to-text/docs/transcribe-client-libraries#client-libraries-install-java
+//Rest API Reference Material - https://cloud.google.com/speech-to-text/docs/reference/rest/v1/RecognitionConfig
+// Imports the Google Cloud client library
+import com.google.cloud.speech.v1.RecognitionAudio;
+import com.google.cloud.speech.v1.RecognitionConfig;
+import com.google.cloud.speech.v1.RecognitionConfig.AudioEncoding;
+import com.google.cloud.speech.v1.RecognizeResponse;
+import com.google.cloud.speech.v1.SpeechClient;
+import com.google.cloud.speech.v1.SpeechRecognitionAlternative;
+import com.google.cloud.speech.v1.SpeechRecognitionResult;
+import java.util.List;
+
+
+public class QuickstartSample {
+    /** Demonstrates using the Speech API to transcribe an audio file. */
+    public static void main(String... args) throws Exception {
+        // Instantiates a client
+        try (SpeechClient speechClient = SpeechClient.create()) {
+
+            // The path to the audio file to transcribe.   This is a GCloud Bucket
+            String gcsUri = "gs://cloud-samples-data/speech/brooklyn_bridge.raw";
+
+            // Builds the sync recognize request
+            RecognitionConfig config =
+                    RecognitionConfig.newBuilder()
+                            .setEncoding(AudioEncoding.LINEAR16)
+                            .setSampleRateHertz(16000)
+                            .setLanguageCode("en-US")
+                            .build();
+            RecognitionAudio audio = RecognitionAudio.newBuilder()
+                    .setUri(gcsUri)
+                    .build();
+
+            // Performs speech recognition on the audio file
+            RecognizeResponse response = speechClient.recognize(config, audio);
+            List<SpeechRecognitionResult> results = response.getResultsList();
+
+            for (SpeechRecognitionResult result : results) {
+                // There can be several alternative transcripts for a given chunk of speech. Just use the
+                // first (most likely) one here.
+                SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
+                System.out.printf("Transcription: %s%n", alternative.getTranscript());
+            }
+        }
+    }
+}
